e2a6d26a5b5dd058287ccca8cfc03640d87ff65f
at3c <bogdan_s86@ukr.net>
2018-11-08 23:33:30 +0200

    finish method to test menu items, add comments for javadoc

--- src/test/java/com/qatestlab/hw2/tests/AdminTest.java
+++ src/test/java/com/qatestlab/hw2/tests/AdminTest.java
@@ -1,79 +1,114 @@
 package com.qatestlab.hw2.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Class to test web site with admin panel
- * @autor Admin on 04.11.2018.
- * version 1.0
+ * @autor Admin on 08.11.2018.
+ * version 1.1
  */
 public class AdminTest {
+
     public static void main(String[] args) throws InterruptedException {
         // create instace "webdriver" for testing website
         WebDriver driver = getWebDriver();
-        //Boolean resultTestAdminPanel = adminPanelTest(driver); //test login/logout web site
-        Boolean resultTestAdminMenu = menuPanelTest(driver);
-        //System.out.println("Test admin panel: " + resultTestAdminPanel);
-        //System.out.println("Test admin panel Menu: " + resultTestAdminMenu);
+        adminPanelTest(driver); //test login/logout web site
+        System.out.println("Test admin panel was finished");
+        driver = getWebDriver();
+        menuPanelTest(driver);
+        System.out.println("Test admin panel menu was finished");
     }
 
-//    public static Boolean adminPanelTest(WebDriver driver) throws InterruptedException {
-//        logIn(driver);
-//        WebElement userMenu = driver.findElement(By.className("employee_name"));
-//        userMenu.click();
-//        Thread.sleep(2000);
-//
-//        WebElement logout = driver.findElement(By.id("header_logout"));
-//        logout.click();
-//        Thread.sleep(1000);
-//
-//        // close browser
-//        driver.quit();
-//
-//        return true;
-//    }
-
-    public static Boolean menuPanelTest(WebDriver driver) throws InterruptedException {
+    /**
+     * test login as Admin and logout with help admin panel
+     * @param driver  object to use browser as user
+     * @throws InterruptedException
+     */
+    public static void adminPanelTest(WebDriver driver) throws InterruptedException {
         logIn(driver);
-        Thread.sleep(3000);
-        //List<WebElement> menuItems = (ArrayList<WebElement>) driver.findElements(By.partialLinkText("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/index.php?controller=Admin"));
-        List<WebElement> elemItems = driver.findElements(By.className("has_submenu"));
-        for (WebElement item: elemItems) {
-            System.out.println(item.getText());
-//            WebElement aItem = item.findElement(By.tagName("a"));
-//            System.out.println(aItem.toString());
-        }
-//        for (WebElement item : menuItems) {
-//            item.click();
-//            Thread.sleep(2000);
-//            WebElement titleBeforeRefresh = driver.findElement(By.className("page-title"));
-//            System.out.println(titleBeforeRefresh.getText());
-//            driver.navigate().refresh();
-//            Thread.sleep(2000);
-//            WebElement titleAfterRefresh = driver.findElement(By.className("page-title"));
-//            if (titleBeforeRefresh.getText().equals(titleAfterRefresh.getText())) {
-//                System.out.println("After refresh we stay at the web page: " + titleBeforeRefresh.getText());
-//            } else {
-//                System.out.println(("After refresh web site opened another page with title: " + titleAfterRefresh.getText()));
-//            }
-//        }
-        return true;
+
+        WebElement userMenu = driver.findElement(By.className("employee_name"));
+        userMenu.click();
+        Thread.sleep(2000);
+
+        WebElement logout = driver.findElement(By.id("header_logout"));
+        logout.click();
+        Thread.sleep(1000);
+
+        // close browser
+        driver.quit();
     }
 
-    // getter to instance WebDriver
+    /**
+     *  login as Admin and test menu items
+     * @param driver object to use browser as user
+     * @throws InterruptedException
+     */
+    public static void menuPanelTest(WebDriver driver) throws InterruptedException {
+        logIn(driver);
+        Thread.sleep(2000);
+
+        // initiation ArrayList object with values of menu items
+        List<String>  menuItems = new ArrayList<String>(Arrays.asList("Dashboard", "Заказы", "Каталог", "Клиенты", "Служба поддержки",
+                "Статистика", "Modules","Design","Доставка","Способ оплаты","International","Shop Parameters","Конфигурация"));
+
+        // iterate object items
+        for (String item: menuItems) {
+            WebElement elemItems = driver.findElement(By.linkText(item));
+            elemItems.click();
+            Thread.sleep(1000);
+            WebElement titlePage;
+            try {
+                titlePage = driver.findElement(By.className("page-title"));
+            } catch (NoSuchElementException e) {
+                System.err.println("Title wasn't on the page: " + item);
+                continue;
+            } catch (Exception e) {
+                System.err.println(e.toString());
+                continue;
+            }
+                String titlePageBeforeRefresText = titlePage.getText();
+                System.out.println(titlePageBeforeRefresText);
+                driver.navigate().refresh();
+                Thread.sleep(1000);
+                WebElement titlePageAfterRefresh = driver.findElement(By.className("page-title"));
+
+                //display results to the console
+                if (!(titlePageBeforeRefresText.equals(titlePageAfterRefresh.getText()))) {
+                    System.out.println("The page was refreshed. You didn't stay in the same section");
+                }
+                System.out.println("The page was refreshed. You stayed in the same section: " +
+                        titlePageAfterRefresh.getText());
+        }
+        // close browser
+        driver.quit();
+    }
+
+    /**
+     * method set url to web browser's driver,
+     * create connection to browser
+     * @return  Chrome driver object to use browser as user
+     */
     public static WebDriver getWebDriver() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//resources//chromedriver.exe");
         return new ChromeDriver();
     }
 
+    /**
+     * Login to a Website as an Admin
+     * @param driver object needed to connection to the web browser
+     * @throws InterruptedException
+     */
     private static void logIn(WebDriver driver) throws InterruptedException {
-        //get web site
+        //get test web site
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
         Thread.sleep(2000);
 
